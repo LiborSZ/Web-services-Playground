34c41b5bdd8589ef9ccfebd5cd78a428baed2f1b
Libor <Szucs>
2024-07-06 07:15:26 +0200

    Products controller / EF core nugget

--- API/Web API playground/Controllers/ProductsController.cs
+++ API/Web API playground/Controllers/ProductsController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using WebApiData;
+using WebApiModels.Models;
 
 namespace Web_API_playground.Controllers
 {
@@ -8,39 +11,85 @@ namespace Web_API_playground.Controllers
 
     public class ProductsController : ControllerBase
     {
-        private static readonly List<string> products = new List<string>
+        private readonly ApiDbContext _context;
+
+        public ProductsController(ApiDbContext context)
         {
-            "Mobiles", "Notebooks", "Desktop PC's"
-        };
+            _context = context;
+        }
 
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
         {
-            return products;
+            return await _context.Products.ToListAsync();
         }
 
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            if (id < 0 || id >= products.Count)
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
             {
                 return NotFound();
             }
-            return products[id];
+            return product;
         }
 
         [HttpPost]
-        public void Post([FromBody] string product)
+        public async Task<ActionResult<Product>> PostProduct(Product product)
         {
-            products.Add(product);
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
         }
 
-        [HttpDelete]
-        public void Delete(int id)
+        [HttpPut]
+        public async Task<IActionResult> PutProduct(int id, Product product)
         {
-            if(id >= 0 && id < products.Count)
-            products.RemoveAt(id);
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DBConcurrencyException e)
+            {
+                if (!ProductExists(product.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw new Exception(e.Message);
+                }
+            }
+            return NoContent();
         }
 
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(e => e.Id == id);
+        }
     }
 }
